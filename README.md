b88ea86968d174e86a3040e59ed3c863864de7a4
Greg C <gregjc@protonmail.com>
2024-01-08 21:39:45 +1300

    Add argparse and a readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Usage
+1. Configure Substance Painter (or your texturing program) to export textures and an idmap.
+2. Run `python3 main.py` in the root directory of this project.
+
+# Substance Painter
+## 1: Setup an export template like the following
+ 
+![img.png](img.png)
+Note:
+1. There is an idmap exported, which is used to determine which texture belongs to which object.
+2. UDIMS (if used) are sorted into folders (and the script needs to be run against each folder separately, no UDIM support yet)
+
+## 2: Run the script
+```
+python3 main.py ./exampledata/udim/1001/*.png ./mytextures
+```
+This will spit out a texture for each type of texture (basecolor, normal, etc) in the `mytextures` folder.
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import re
 from PIL import Image
 from returns.result import Result, Success, Failure
+import argparse
 
 groupregex = re.compile(u'.*_(diffuse|basecolor|normal|height|specular|glossiness).*', re.IGNORECASE)
 idregex = re.compile(u'.*(id).*', re.IGNORECASE)
@@ -95,9 +96,9 @@ def combine(filenames: list[(str, str)], fname: str, outdir: str) -> Result(list
         outfile.save(path)
 
         return Success(path)
-def main(fileglob: str, outdir: str):
-    os.makedirs(outdir, exist_ok=True)
 
+def start(fileglob: str, outdir: str):
+    os.makedirs(outdir, exist_ok=True)
 
     filenames = get_input_files(fileglob)
     sets = make_sets(filenames)
@@ -106,5 +107,13 @@ def main(fileglob: str, outdir: str):
     for (groupname, filenames) in sets.items():
         print(combine(filenames, groupname, outdir))
 
+def main():
+    parser = argparse.ArgumentParser(description='Combine textures from multiple maps into a single texture')
+    parser.add_argument('input', metavar='input', type=str, help='glob to find files')
+    parser.add_argument('outdir', metavar='outdir', type=str, help='output directory')
+    args = parser.parse_args()
+
+    start(args.input, args.outdir)
+
 if __name__ == '__main__':
-    main(u"./exampledata/udim/1001/*.png", "./out")
+    main()
