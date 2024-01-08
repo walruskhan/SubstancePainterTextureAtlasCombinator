cc013cde4dc5d83f491781912b341f78a9950af9
Greg C <gregjc@protonmail.com>
2024-01-09 00:32:17 +1300

    Handle udims

--- src/main.py
+++ src/main.py
@@ -69,10 +69,11 @@ class TextureSet:
         self._textures: dict[str, list(Texture)] = {} # a dictionary of materials -> textures (e.g. [body,id], [head,id])
                                                       # we use t.name_minus_type as the key because it handles UDIMS / other stuff succinctly
         self._type = type
+        self._udim = None
         self._size = None
 
     def __repr__(self):
-        return f"<TextureSet type='{self.type}' count='{len(self._textures.keys())}' />"
+        return f"<TextureSet type='{self.type}' udim='{self._udim}' count='{len(self._textures.keys())}' />"
 
     @property
     def type(self) -> Result[str, None]:
@@ -93,12 +94,18 @@ class TextureSet:
         if t.type != self._type:
             raise Exception("Cannot add texture with different type than textureset")
 
+        if self._udim is not None and t.udim != self._udim:
+            raise Exception("Cannot add texture with different udim ident than textureset")
+
         # Ensure textures have the same size (e.g. all diffuse textures should be the same size)
         if self._size is None:
             self._size = t.size
         else:
             if t.size != self._size:
                 raise Exception("Cannot add texture with different size than textureset")
+
+        if self._udim is None:
+            self._udim = t.udim
 
         # Create sub-list for this material if it doesn't exist
         if t.nominal_name not in self._textures:
@@ -107,6 +114,8 @@ class TextureSet:
         self._textures[t.nominal_name].append(t)
 
     def write_to(self, idmaps, outdir):
+        udim = None
+
         with Image.new("RGB", self._size, (0, 0, 0)) as outfile:
             for (nominal, textures) in self._textures.items():
                 # Get idmap to use with writing current nominal textures
@@ -114,6 +123,7 @@ class TextureSet:
                 if idmap is Failure:
                     raise Exception(f"Could not find idmap for {nominal}")
                 idmap = idmap.unwrap()
+                udim = idmap.udim
 
                 with Image.open(idmap.filename) as idmapfile:
                     for texture in textures:
@@ -122,12 +132,17 @@ class TextureSet:
                             for y in range(self._size[1]):
                                 for x in range(self._size[0]):
                                     pixel = idmapfile.getpixel((x, y))
-                                    if pixel[2] == 0:
+
+                                    if type(pixel) is tuple and pixel[2] == 0:
+                                        continue
+
+                                    if type(pixel) is int and pixel == 0:
                                         continue
 
                                     outfile.putpixel((x, y), texturefile.getpixel((x, y)))
 
-            path = os.path.join(outdir, f"{self._type}.png")
+            udim_segment = f".{udim}" if udim else ""
+            path = os.path.join(outdir, f"{self._type}{udim_segment}.png")
             print(f"Wrote {path}")
             outfile.save(path)
 
@@ -155,9 +170,10 @@ def make_texturesets(textures: list[Texture]) -> (dict[str, TextureSet], list[Te
             continue
 
         # If this is not an idmap, add it to the textureset list
+        key = (texture.type, texture.udim)
         if texture.type not in sets:
-            sets[texture.type] = TextureSet(texture.type)
-        sets[texture.type].add(texture)
+            sets[key] = TextureSet(texture.type)
+        sets[key].add(texture)
 
     return sets, idmaps
 
